06ed46d563d0b49624cecc13fce0b8401a01d5e0
Brandon Fulljames <bfullj@gmail.com>
2017-09-05 19:16:29 -0700

    Successfully answer first question

--- index.js
+++ index.js
@@ -11,9 +11,28 @@ const parser = csv.parse({}, function(err, data){
   sightings = data
     .slice(1)
     .map(vals => new Sighting(vals))
-    .filter(s => !isNaN(s.latitude));
+    .filter(s => !!s.dateOccurred);
 
-  console.log(sightings[0].dateOccurred.year());
+  // What year had the most sightings?
+  const yearCounts = sightings.reduce((a, s) => {
+    const year = s.dateOccurred.year();
+    a[year] = a[year] || 0;
+    a[year]++;
+    return a;
+  }, {});
+
+  const max = Object.keys(yearCounts).reduce((a, key) => {
+    if (!a || a.count < yearCounts[key]) {
+      a = {
+        year: key,
+        count: yearCounts[key],
+      };
+    }
+
+    return a;
+  }, null);
+
+  console.log('Most sightings in year ' + max.year + ' - ' + max.count);
 });
 
 fs.createReadStream('scrubbed.csv').pipe(parser);
